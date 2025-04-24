062ea07b2e28f27201d320f5481a86ae6c103349
John Doe <g_anz@mail.ru>
2025-04-24 21:19:38 +0300

    meters and valves

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const MongoClient = require("mongodb").MongoClient;
 const mqtt = require("mqtt");
 
 let db;
+let broker;
 const mongoc = new MongoClient(`mongodb://mongo:27017/`);
 mongoc.connect().then(mc => {
   console.log("Подключение к mongo установлено");
@@ -20,17 +21,33 @@ const wss = new WebSocketServer({ port: 7000 });
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    
+  ws.on('message', function message(r) {
+    res = JSON.parse(r);
+
+    if (res.event == "get/valves/time/request") {
+      getValvesData(ws);
+      setImmediate(() => getValvesData(ws));
+    }
+
+    if (res.event == "set/valve/time/request") {
+      broker.publish("vk" + res.data.valve, JSON.stringify(res.data.time), { qos: 0, retain: false }, (error) => {
+        if (error) {
+          console.error("brocker publish fail", error);
+        }
+      })
+    }
+
+    if (res.event == "getMetersData") {
+      getMetersData(ws);
+      let interval = setInterval(() => getMetersData(ws), 10000);
+    }
 
   });
 
-  getSelPos(ws);
-  let interval = setInterval(() => getSelPos(ws), 10000);
+
 });
 
-const getSelPos = async (ws) => {
+const getMetersData = async (ws) => {
   // name value
   const h1 = await db.collection("humidity1").find().sort({ _id: -1 }).limit(1).toArray()
   const h2 = await db.collection("humidity2").find().sort({ _id: -1 }).limit(1).toArray()
@@ -41,6 +58,21 @@ const getSelPos = async (ws) => {
   ws.send(JSON.stringify([].concat(h1, h2, h3, h4, h5)))
 }
 
+const getValvesData = async (ws) => {
+  const v1 = await db.collection("valves").find({ name: "1" }).sort({ _id: -1 }).limit(1).toArray()
+  const v2 = await db.collection("valves").find({ name: "2" }).sort({ _id: -1 }).limit(1).toArray()
+  const v3 = await db.collection("valves").find({ name: "3" }).sort({ _id: -1 }).limit(1).toArray()
+  const v4 = await db.collection("valves").find({ name: "4" }).sort({ _id: -1 }).limit(1).toArray()
+  const data = [
+    { valve: 1, time: v1 ? parseInt(v1[0].time) : 0 },
+    { valve: 2, time: v2 ? parseInt(v2[0].time) : 0 },
+    { valve: 3, time: v3 ? parseInt(v3[0].time) : 0 },
+    { valve: 4, time: v4 ? parseInt(v4[0].time) : 0 },
+  ]
+
+  ws.send(JSON.stringify({ event: "get/valves/time/response", data }))
+}
+
 const brokerIp = process.env.BROKER_IP;
 const protocol = "mqtt";
 const portMqtt = "1883";
@@ -48,7 +80,7 @@ const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
 const connectUrl = `${protocol}://${brokerIp}:${portMqtt}`;
 
-const broker = mqtt.connect(connectUrl, {
+broker = mqtt.connect(connectUrl, {
   clientId,
   clean: true,
   connectTimeout: 4000,
@@ -65,10 +97,16 @@ broker.on("connect", () => {
     broker.subscribe("humidity3");
     broker.subscribe("humidity4");
     broker.subscribe("humidity5");
+    broker.subscribe("vk1o");
+    broker.subscribe("vk2o");
+    broker.subscribe("vk3o");
+    broker.subscribe("vk4o");
   }
 });
 
 broker.on('message', (topic, message) => {
+  console.log(topic);
+  
   if (topic == "humidity1") {
     db.collection("humidity1").insertOne({ name: "humidity1", value: message.toString() });
   }
@@ -84,7 +122,18 @@ broker.on('message', (topic, message) => {
   if (topic == "humidity5") {
     db.collection("humidity5").insertOne({ name: "humidity5", value: message.toString() });
   }
+  if (topic == "vk1o" || topic == "vk2o" || topic == "vk3o" || topic == "vk4o") {
+    handleValve(topic, message);
+  }
 });
+
+function handleValve(topic, message) {
+  const name = topic.slice(2, 3);
+  const time = message.toString();
+
+  db.collection("valves").insertOne({ name, time });
+  wss.emit(JSON.stringify({ event: "set/valve/time/response", time }));
+}
 
 broker.on("error", (error) => {
   console.error("connection failed", error);
--- cli/src/pages/Valves.jsx
+++ cli/src/pages/Valves.jsx
@@ -1,52 +1,103 @@
-import useWebSocket, { ReadyState } from "react-use-websocket"
-import React, { useState, useEffect } from 'react';
-
+import useWebSocket, { ReadyState } from "react-use-websocket";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Valves() {
-    const WS_URL = "ws://localhost:7000"
-    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
-        WS_URL,
-        {
-            share: false,
-            shouldReconnect: () => true,
-        },
-    )
+	const [valves, setValves] = useState([]);
+	const WS_URL = `ws://${process.env.REACT_APP_API_IP}:7000`;
+	const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+		WS_URL,
+		{
+			share: false,
+			shouldReconnect: () => true,
+		}
+	);
 
-    // Run when the connection state (readyState) changes
-    useEffect(() => {
-        console.log("Connection state changed")
-        if (readyState === ReadyState.OPEN) {
-            sendJsonMessage({
-                event: "subscribe",
-                data: {
-                    channel: "general-chatroom",
-                },
-            })
-        }
-    }, [readyState])
+	// Handle WebSocket connection state
+	useEffect(() => {
+		if (readyState === ReadyState.OPEN) {
+			sendJsonMessage({
+				event: "get/valves/time/request", data: {}
+			});
+		}
+	}, [readyState]);
 
-    // Run when a new WebSocket message is received (lastJsonMessage)
-    useEffect(() => {
-        console.log(`Got a new message: ${JSON.stringify(lastJsonMessage)}`)
-    }, [lastJsonMessage])
+	//Handle incoming WebSocket messages
+	useEffect(() => {
+		if (lastJsonMessage) {
+			if (lastJsonMessage.event === "get/valves/time/response")
+				setValves([...lastJsonMessage.data]); // Assuming the response is an array of objects
+				console.log(lastJsonMessage.data);
+				
+		}
+	}, [lastJsonMessage]);
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>
-                    Длительность полива
-                </h1>
-            </header>
-            <div>
-                <ul>
-                    <li><span>Клаппанная группа 1</span><span> 0 </span><span> 0 </span><button>Сохранить</button></li>
-                    <li><span>Клаппанная группа 2</span><span> 0 </span><span> 0 </span><button>Сохранить</button></li>
-                    <li><span>Клаппанная группа 3</span><span> 0 </span><span> 0 </span><button>Сохранить</button></li>
-                    <li><span>Клаппанная группа 4</span><span> 0 </span><span> 0 </span><button>Сохранить</button></li>
-                </ul>
-                <div>Общее время полива <span> 0 </span></div>
-            </div>
-        </div>
-    );
+	const handleTimeChange = (index, type, value) => {
+		const updatedTimes = [...valves];
+		updatedTimes[index - 1].time = value;
+		// setValves(updatedTimes);
+
+	};
+
+	const saveTime = (index) => {
+		const time = valves[index - 1].time;
+		sendJsonMessage({
+			event: "set/valve/time/request",
+			data: {
+				valve: index,
+				time,
+			}
+		})
+	};
+
+	return (
+		<div className="container mt-5">
+			<header className="text-center mb-4">
+				<h1>Длительность полива</h1>
+			</header>
+			<div>
+				<ul className="list-group">
+					{valves.map(({ valve, time }, index) => (
+
+						<li
+							key={index}
+							className="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-center"
+						>
+							<div className="d-flex align-items-center mb-2 mb-md-0">
+								<span className="me-2">Клапанная группа {valve}</span>
+								<span className="badge bg-secondary">{time}</span>
+							</div>
+							<div className="d-flex align-items-center">
+								<input
+									type="number"
+									className="form-control me-2"
+									style={{ width: "80px" }}
+									placeholder="Минуты"
+									// value={time}
+									onChange={(e) => handleTimeChange(valve, "time", parseInt(e.target.value) || 0)}
+									min="0"
+									max="300"
+								/>
+								<button className="btn btn-primary" onClick={() => saveTime(valve)}>
+									Сохранить
+								</button>
+							</div>
+						</li>
+					))}
+				</ul>
+				{/* <div className="mt-4 text-center">
+					<h5>
+						Общее время полива:{" "}
+						{times.reduce(
+							(total, time) => total + time,
+							0
+						)}{" "}
+						минут
+					</h5>
+				</div> */}
+			</div>
+		</div>
+	);
 }
+
 export default Valves;
