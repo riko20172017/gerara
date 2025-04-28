a9649848c30d01dcf4aa6ad09d43c52aa3cecb35
anz <g_anz@mail.ru>
2025-04-28 20:47:52 +0300

    обновить компонент Pamps: заменить на Valves, добавить обработку WebSocket для клапанов и улучшить отображение

--- cli/src/pages/Pamps.jsx
+++ cli/src/pages/Pamps.jsx
@@ -1,43 +1,88 @@
+import useWebSocket, { ReadyState } from "react-use-websocket";
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Pamps() {
+function Valves() {
+  const [disabled, setDisabled] = useState([]);
+  const [valves, setValves] = useState([]);
+
+  const WS_URL = `ws://${process.env.REACT_APP_API_IP}:7000`;
+  const {
+    sendJsonMessage: send,
+    lastJsonMessage: message,
+    readyState,
+  } = useWebSocket(WS_URL, {
+    share: false,
+    shouldReconnect: () => true,
+  });
+
+  // Handle WebSocket connection state
+  useEffect(() => {
+    if (readyState === ReadyState.OPEN) {
+      send({
+        event: "get/valves/time/request",
+        data: {},
+      });
+    }
+  }, [readyState]);
+
+  //Handle incoming WebSocket messages
+  useEffect(() => {
+    if (message && message.event && message.data) {
+      const { event, data } = message;
+      console.log(event, data);
+      if (event === "get/valves/time/response") setValves([...data]); // Assuming the response is an array of objects
+      if (event === "valve/time") {
+        const nextValves = valves.map((v, i) => {
+          if (v.name === data.name) {
+            return data;
+          } else {
+            return v;
+          }
+        });
+        setValves(nextValves);
+      }
+    }
+  }, [message]);
+
+  const handleClick = (name, time) => {
+    send({
+      event: "set/valve/time/request",
+      data: { name, time },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Длительность полива</h1>
+    <div className="container mt-5">
+      <header className="text-center mb-4">
+        <h1>Насосы</h1>
       </header>
       <div>
-        <ul>
-          <li>
-            <span>Клаппанная группа 1</span>
-            <span> 0 </span>
-            <span> 0 </span>
-            <button>Сохранить</button>
-          </li>
-          <li>
-            <span>Клаппанная группа 2</span>
-            <span> 0 </span>
-            <span> 0 </span>
-            <button>Сохранить</button>
-          </li>
-          <li>
-            <span>Клаппанная группа 3</span>
-            <span> 0 </span>
-            <span> 0 </span>
-            <button>Сохранить</button>
-          </li>
-          <li>
-            <span>Клаппанная группа 4</span>
-            <span> 0 </span>
-            <span> 0 </span>
-            <button>Сохранить</button>
-          </li>
+        <ul className="list-group">
+          {valves.map(({ name, time }, index) => (
+            <li
+              key={index}
+              className="list-group-item d-flex flex-row flex-md-row justify-content-between align-items-center"
+            >
+              <div className="d-flex align-items-center mb-2 mb-md-0">
+                <h4 className="me-2 position-relative">
+                  Насос {name}
+                </h4>
+              </div>
+              <div>
+                {/* <button type="button" class="btn btn-primary">
+                  Вкл
+                </button> */}
+                <button type="button" class="btn btn-light">
+                  Выкл
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
-        <div>
-          Общее время полива <span> 0 </span>
-        </div>
       </div>
     </div>
   );
 }
-export default Pamps;
+
+export default Valves;
