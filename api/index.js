62a399748d0a81a4aa1f77a1e98004f2ca945dc5
anz <g_anz@mail.ru>
2025-06-18 23:22:18 +0300

    add gerara.ru for local

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 const { WebSocketServer } = require("ws");
-const { createServer } = require("https");
-const { readFileSync } = require("fs");
+const { createServer } = require("http");
 const express = require("express"); // подключаем фреймворк Express (модуль)
 const MongoClient = require("mongodb").MongoClient;
 const mqtt = require("mqtt");
@@ -8,10 +7,7 @@ const setupMqttClient = require("./modules/broker/broker.js");
 const mqttInits = require("./modules/broker/options.js");
 const setupWebSocket = require("./modules/socket/socket.js");
 
-const server = createServer({
-  cert: readFileSync('/etc/letsencrypt/live/gerara.ru/cert.pem'),
-  key: readFileSync('/etc/letsencrypt/live/gerara.ru/privkey.pem')
-});
+const server = createServer({ noServer: true });
 
 async function setup() {
   try {
@@ -20,12 +16,19 @@ async function setup() {
     console.log("1. Подключение к mongo установлено");
 
     const db = connection.db("gerara");
-    const wss = new WebSocketServer({ server });
+    const wss = new WebSocketServer({ noServer: true });
+
+    server.on("upgrade", (req, socket, head) => {
+      wss.handleUpgrade(req, socket, head, (ws) => {
+        wss.emit("connection", ws, req);
+      });
+    });
+
     const mqttClient = mqtt.connect(mqttInits.url, mqttInits.options);
 
     setupMqttClient(mqttClient, wss, db); // инициализируем брокер mqtt
     setupWebSocket(mqttClient, wss, db); // инициализируем WebSocket
-    server.listen(7000)
+    server.listen(7000);
   } catch (err) {
     console.error("Ошибка подключения к MongoDB:", err);
   }
@@ -33,27 +36,27 @@ async function setup() {
 
 setup();
 
-const app = express(); // создаем экземпляр приложения
-const router = express.Router(); // создаем экземпляр роутера
+// const app = express(); // создаем экземпляр приложения
+// const router = express.Router(); // создаем экземпляр роутера
 
-const path = __dirname; // записываем путь до рабочего каталога
-const port = 8000; // записываем порт сервера
+// const path = __dirname; // записываем путь до рабочего каталога
+// const port = 8000; // записываем порт сервера
 
-// выводим в консоль HTTP METHOD при каждом запросе
-router.use(function (req, res, next) {
-  console.log("/" + req.method);
-  next();
-});
+// // выводим в консоль HTTP METHOD при каждом запросе
+// router.use(function (req, res, next) {
+//   console.log("/" + req.method);
+//   next();
+// });
 
-// отвечаем на запрос главной страницы файлом index.html
-router.get("/", function (req, res) {
-  res.sendFile(path + "/index.html");
-});
+// // отвечаем на запрос главной страницы файлом index.html
+// router.get("/", function (req, res) {
+//   res.sendFile(path + "/index.html");
+// });
 
-// подключаем роутер к приложению
-app.use("/", router);
+// // подключаем роутер к приложению
+// app.use("/", router);
 
-// начинаем прослушивать порт 8080, тем самым запуская http-сервер
-app.listen(port, function () {
-  console.log("Listening on port 8000");
-});
+// // начинаем прослушивать порт 8080, тем самым запуская http-сервер
+// app.listen(port, function () {
+//   console.log("Listening on port 8000");
+// });
